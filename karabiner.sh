4e398b27ced99c2654d2e3e8f1ddee2283e21931
jiangxd <jiangxd2016@gmail.com>
2022-08-16 20:27:43 +0800

    chore: window keymap

--- karabiner.sh
+++ karabiner.sh
@@ -4,6 +4,7 @@ KAR_DIR="$HOME/.config/karabiner/assets/complex_modifications"
 if [ -d KAR_DIR ]
   ln -sf $PWD/karabiner/* $KAR_DIR
 then
+    mkdir -p $KAR_DIR
     echo 'karabiner config already exists'
 fi
     
