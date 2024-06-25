fbdbdc3d3961c145d2beb84c644977723a12f3a3
jiangxd <jiangxd2016@gmail.com>
2024-06-25 09:39:53 +0800

    feat: update nivm config

--- nvim.sh
+++ nvim.sh
@@ -14,11 +14,15 @@ git config --global https.https://github.com.proxy socks5://127.0.0.1:7890
 echo $NVIM_DIR
 echo $PWD
 
+
+sudo rm -r ~/.local/share/nvim/
+sudo rm -r ~/.local/state/nvim/
+
 rm -rf $NVIM_DIR/nvim
 mkdir -p $NVIM_DIR
 
-rm -rf $NVIM_DIR/kitty
-mkdir -p $NVIM_DIR/kitty
-ln -sf  ./kitty/* $NVIM_DIR/kitty/
+# rm -rf $NVIM_DIR/kitty
+# mkdir -p $NVIM_DIR/kitty
+# ln -sf  ./kitty/* $NVIM_DIR/kitty/
 
 cp -R $PWD/nvim $NVIM_DIR
